0c2878f9b4bb26fcbc5aac0a490ccc265544d818
Kovács Dorina <kovacsdorina2@gmail.com>
2022-03-01 08:57:41 +0100

    Kezdes

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---------------------");
+        System.out.println();
+        System.out.println("1) Program Inditás");
+        System.out.println();
+        System.out.println("2) Nevjegy");
+        System.out.println();
+        System.out.println("3) Kilep");
+
+        System.out.println();
+        System.out.println();
+        System.out.println("Választás: ");
+        String valasztott = sc.nextLine();
+    }
+}
